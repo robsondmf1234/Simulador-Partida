c4c6c4705ccfde97688ac585b00c8cca816242a8
robsondmf1234@gmail.com <Itupeva123@>
2022-04-08 11:39:57 -0300

    Desenvolvimento de layouts de listagem e detalhamento

---
+++ app/src/main/java/com/example/simulador/DetailActivity.kt
@@ -0,0 +1,19 @@
+package com.example.simulador
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.simulador.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
+
